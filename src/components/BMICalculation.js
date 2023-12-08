e2b42ee0ec0076b448dcfa525ae7c47dc3e37b09
bad02 <badoniyashivam@gmail.com>
2023-12-08 22:12:10 +0530

    Bug Fix: Input Validation and BMI Calculation

--- src/components/BMICalculation.js
+++ src/components/BMICalculation.js
@@ -4,17 +4,46 @@ function BMICalculation() {
     const [weight, setWeight] = useState('');
     const [height, setHeight] = useState('');
     const [bmi, setBMI] = useState(null);
+    const [error, setError] = useState(null);
+    const [bmiMessage, setBmiMessage] = useState(''); 
 
     const calculateBMI = () => {
         const validWeight = parseFloat(weight);
         const validHeight = parseFloat(height);
 
-        if (!isNaN(validWeight) && !isNaN(validHeight)) {
-            const calculatedBMI = validWeight / Math.pow(validHeight, 2);
-            setBMI(calculatedBMI.toFixed(1));
-        } else {
+        if (
+            isNaN(validWeight) ||
+            isNaN(validHeight) ||
+            validWeight < 1.0 || validWeight > 300.0 ||
+            validHeight < 0.1 || validHeight > 3.0
+        ) {
+            setError('Please enter valid weight (1.0-300.0 kg) and height (0.1-3.0 m).');
             setBMI(null);
+            return;
         }
+
+        const calculatedBMI = validWeight / Math.pow(validHeight, 2);
+
+        if (isNaN(calculatedBMI) || calculatedBMI <= 0) {
+            setError('Invalid input. Please check your weight and height.');
+            setBMI(null);
+            return;
+        }
+
+        setError(null);
+        setBMI(calculatedBMI.toFixed(1));
+         // Set BMI message based on calculated BMI
+         let message = '';
+         if (calculatedBMI < 18.5) {
+             message = 'You are Underweight';
+         } else if (calculatedBMI >= 18.5 && calculatedBMI < 25) {
+             message = 'You are Normal weight';
+         } else if (calculatedBMI >= 25 && calculatedBMI < 30) {
+             message = 'You are Overweight';
+         } else {
+             message = 'You are Obese';
+         }
+         setBmiMessage(message);
     };
 
     return (
@@ -23,17 +52,33 @@ function BMICalculation() {
             <div>
                 <label>
                     Weight (kg):
-                    <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+                    <input
+                        type="number"
+                        value={weight}
+                        onChange={(e) => setWeight(e.target.value)}
+                        min="1.0"
+                        max="300.0"
+                        step="0.1"
+                    />
                 </label>
             </div>
             <div>
                 <label>
                     Height (m):
-                    <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} />
+                    <input
+                        type="number"
+                        value={height}
+                        onChange={(e) => setHeight(e.target.value)}
+                        min="0.1"
+                        max="3.0"
+                        step="0.01"
+                    />
                 </label>
             </div>
             <button onClick={calculateBMI}>Calculate BMI</button>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
             {bmi !== null && <p>Your BMI: {bmi}</p>}
+            {bmiMessage && <p>{bmiMessage}</p>}
         </div>
     );
 }
